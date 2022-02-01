ea501c72521f472fa597e6ba86b4428fc43d59d9
ViserVersa <98088676+ViserVersa@users.noreply.github.com>
2022-02-01 21:01:53 +0100

    test

--- Klick/Form1.Designer.cs
+++ Klick/Form1.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.btn_Update = new System.Windows.Forms.Button();
             this.lbl_Update = new System.Windows.Forms.Label();
-            this.btn_Reset = new System.Windows.Forms.Button();
             this.txt_X = new System.Windows.Forms.TextBox();
             this.txt_Y = new System.Windows.Forms.TextBox();
-            this.rbtn_Repeat = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_X_Next = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_Update
@@ -54,52 +55,59 @@
             this.lbl_Update.Size = new System.Drawing.Size(0, 13);
             this.lbl_Update.TabIndex = 1;
             // 
-            // btn_Reset
-            // 
-            this.btn_Reset.Location = new System.Drawing.Point(658, 368);
-            this.btn_Reset.Name = "btn_Reset";
-            this.btn_Reset.Size = new System.Drawing.Size(75, 23);
-            this.btn_Reset.TabIndex = 2;
-            this.btn_Reset.Text = "Reset";
-            this.btn_Reset.UseVisualStyleBackColor = true;
-            // 
             // txt_X
             // 
-            this.txt_X.Location = new System.Drawing.Point(613, 342);
+            this.txt_X.Location = new System.Drawing.Point(577, 342);
             this.txt_X.Name = "txt_X";
-            this.txt_X.Size = new System.Drawing.Size(83, 20);
+            this.txt_X.Size = new System.Drawing.Size(75, 20);
             this.txt_X.TabIndex = 3;
             this.txt_X.Text = "-500";
             this.txt_X.TextChanged += new System.EventHandler(this.txt_X_TextChanged);
             // 
             // txt_Y
             // 
-            this.txt_Y.Location = new System.Drawing.Point(613, 316);
+            this.txt_Y.Location = new System.Drawing.Point(577, 314);
             this.txt_Y.Name = "txt_Y";
-            this.txt_Y.Size = new System.Drawing.Size(83, 20);
+            this.txt_Y.Size = new System.Drawing.Size(75, 20);
             this.txt_Y.TabIndex = 4;
             this.txt_Y.Text = "300";
             // 
-            // rbtn_Repeat
+            // label1
             // 
-            this.rbtn_Repeat.AutoSize = true;
-            this.rbtn_Repeat.Location = new System.Drawing.Point(577, 397);
-            this.rbtn_Repeat.Name = "rbtn_Repeat";
-            this.rbtn_Repeat.Size = new System.Drawing.Size(60, 17);
-            this.rbtn_Repeat.TabIndex = 5;
-            this.rbtn_Repeat.TabStop = true;
-            this.rbtn_Repeat.Text = "Repeat";
-            this.rbtn_Repeat.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(557, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "X";
+            // 
+            // txt_X_Next
+            // 
+            this.txt_X_Next.Location = new System.Drawing.Point(659, 341);
+            this.txt_X_Next.Name = "txt_X_Next";
+            this.txt_X_Next.Size = new System.Drawing.Size(74, 20);
+            this.txt_X_Next.TabIndex = 8;
+            this.txt_X_Next.Text = "-500";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.rbtn_Repeat);
+            this.Controls.Add(this.txt_X_Next);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Y);
             this.Controls.Add(this.txt_X);
-            this.Controls.Add(this.btn_Reset);
             this.Controls.Add(this.lbl_Update);
             this.Controls.Add(this.btn_Update);
             this.Name = "Form1";
@@ -113,10 +121,11 @@
 
         private System.Windows.Forms.Button btn_Update;
         private System.Windows.Forms.Label lbl_Update;
-        private System.Windows.Forms.Button btn_Reset;
         private System.Windows.Forms.TextBox txt_X;
         private System.Windows.Forms.TextBox txt_Y;
-        private System.Windows.Forms.RadioButton rbtn_Repeat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_X_Next;
     }
 }
 
--- Klick/Form1.cs
+++ Klick/Form1.cs
@@ -13,10 +13,8 @@ namespace Klick
 {
     public partial class Form1 : Form
     {
-
         public const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
         public const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
-
         [DllImport("user32.dll")]
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInf);
         [DllImport("user32.dll")]
@@ -24,32 +22,36 @@ namespace Klick
 
         int Posx;
         int Posy;
+        int PosxNext;
+       
 
         public Form1()
         {
             InitializeComponent(); 
         }
 
-        private void ClickOnLocation (int Posx, int Posy)
+       
+        private void btn_Update_Click(object sender, EventArgs e)
         {
-            
-            
+            Posx = int.Parse(txt_X.Text);
+            Posy = int.Parse(txt_Y.Text);
+            PosxNext = int.Parse(txt_X_Next.Text);
+
+            ClickOnLocation(Posx, Posy);
+              
+        }
+        private void ClickOnLocation(int Posx, int Posy)
+        {
             SetCursorPos(Posx, Posy);
 
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             System.Threading.Thread.Sleep(50);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 
-            lbl_Update.Text = "Updated";
+            lbl_Update.Text = "Updated" + Posx + "  <X Y>  " + Posy;
 
         }
-        private void btn_Update_Click(object sender, EventArgs e)
-        {
-            Posx = int.Parse(txt_X.Text);
-            Posy = int.Parse(txt_Y.Text);
-            ClickOnLocation(Posx, Posy);
-            
-        }
+
         private void txt_X_TextChanged(object sender, EventArgs e)
         {
 
