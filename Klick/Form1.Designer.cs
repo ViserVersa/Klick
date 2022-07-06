1f2537698ed0ede45ef231462424eb37e4a83830
ViserVersa <98088676+ViserVersa@users.noreply.github.com>
2022-07-06 21:57:31 +0200

    XY cords to label start of figures

--- Klick/Form1.Designer.cs
+++ Klick/Form1.Designer.cs
@@ -39,12 +39,18 @@
             this.txt_X_OLD = new System.Windows.Forms.TextBox();
             this.txt_Ymove = new System.Windows.Forms.TextBox();
             this.txt_Xmove = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.dtDataGridView = new System.Windows.Forms.DataGridView();
+            this.txt_SearchLoop = new System.Windows.Forms.NumericUpDown();
+            this.btn_Square = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.txt_LOOP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_SearchLoop)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Update
             // 
-            this.btn_Update.Location = new System.Drawing.Point(73, 199);
+            this.btn_Update.Location = new System.Drawing.Point(73, 114);
             this.btn_Update.Name = "btn_Update";
             this.btn_Update.Size = new System.Drawing.Size(75, 23);
             this.btn_Update.TabIndex = 0;
@@ -62,16 +68,16 @@
             // 
             // txt_X
             // 
-            this.txt_X.Location = new System.Drawing.Point(32, 105);
+            this.txt_X.Location = new System.Drawing.Point(32, 36);
             this.txt_X.Name = "txt_X";
             this.txt_X.Size = new System.Drawing.Size(75, 20);
             this.txt_X.TabIndex = 3;
-            this.txt_X.Text = "300";
+            this.txt_X.Text = "600";
             this.txt_X.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txt_Y
             // 
-            this.txt_Y.Location = new System.Drawing.Point(113, 105);
+            this.txt_Y.Location = new System.Drawing.Point(113, 36);
             this.txt_Y.Name = "txt_Y";
             this.txt_Y.Size = new System.Drawing.Size(75, 20);
             this.txt_Y.TabIndex = 4;
@@ -82,7 +88,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(174, 134);
+            this.label1.Location = new System.Drawing.Point(174, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(14, 13);
             this.label1.TabIndex = 6;
@@ -92,7 +98,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(29, 134);
+            this.label2.Location = new System.Drawing.Point(29, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(14, 13);
             this.label2.TabIndex = 7;
@@ -100,20 +106,20 @@
             // 
             // txt_LOOP
             // 
-            this.txt_LOOP.Location = new System.Drawing.Point(73, 157);
+            this.txt_LOOP.Location = new System.Drawing.Point(73, 88);
             this.txt_LOOP.Name = "txt_LOOP";
             this.txt_LOOP.Size = new System.Drawing.Size(75, 20);
             this.txt_LOOP.TabIndex = 10;
             this.txt_LOOP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txt_LOOP.Value = new decimal(new int[] {
-            2,
+            3,
             0,
             0,
             0});
             // 
             // txt_Y_OLD
             // 
-            this.txt_Y_OLD.Location = new System.Drawing.Point(113, 79);
+            this.txt_Y_OLD.Location = new System.Drawing.Point(113, 10);
             this.txt_Y_OLD.Name = "txt_Y_OLD";
             this.txt_Y_OLD.ReadOnly = true;
             this.txt_Y_OLD.Size = new System.Drawing.Size(75, 20);
@@ -123,7 +129,7 @@
             // 
             // txt_X_OLD
             // 
-            this.txt_X_OLD.Location = new System.Drawing.Point(32, 79);
+            this.txt_X_OLD.Location = new System.Drawing.Point(32, 10);
             this.txt_X_OLD.Name = "txt_X_OLD";
             this.txt_X_OLD.ReadOnly = true;
             this.txt_X_OLD.Size = new System.Drawing.Size(75, 20);
@@ -133,31 +139,76 @@
             // 
             // txt_Ymove
             // 
-            this.txt_Ymove.Location = new System.Drawing.Point(113, 131);
+            this.txt_Ymove.Location = new System.Drawing.Point(113, 62);
             this.txt_Ymove.Name = "txt_Ymove";
             this.txt_Ymove.Size = new System.Drawing.Size(49, 20);
             this.txt_Ymove.TabIndex = 12;
-            this.txt_Ymove.Text = "35";
+            this.txt_Ymove.Text = "0";
             this.txt_Ymove.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txt_Xmove
             // 
-            this.txt_Xmove.Location = new System.Drawing.Point(58, 131);
+            this.txt_Xmove.Location = new System.Drawing.Point(58, 62);
             this.txt_Xmove.Name = "txt_Xmove";
             this.txt_Xmove.Size = new System.Drawing.Size(49, 20);
             this.txt_Xmove.TabIndex = 11;
-            this.txt_Xmove.Text = "35";
+            this.txt_Xmove.Text = "0";
             this.txt_Xmove.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(73, 524);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 15;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // dtDataGridView
+            // 
+            this.dtDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtDataGridView.Location = new System.Drawing.Point(12, 143);
+            this.dtDataGridView.Name = "dtDataGridView";
+            this.dtDataGridView.Size = new System.Drawing.Size(239, 375);
+            this.dtDataGridView.TabIndex = 16;
+            // 
+            // txt_SearchLoop
+            // 
+            this.txt_SearchLoop.Location = new System.Drawing.Point(154, 524);
+            this.txt_SearchLoop.Name = "txt_SearchLoop";
+            this.txt_SearchLoop.Size = new System.Drawing.Size(40, 20);
+            this.txt_SearchLoop.TabIndex = 10;
+            this.txt_SearchLoop.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_SearchLoop.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btn_Square
+            // 
+            this.btn_Square.Location = new System.Drawing.Point(1091, 10);
+            this.btn_Square.Name = "btn_Square";
+            this.btn_Square.Size = new System.Drawing.Size(75, 23);
+            this.btn_Square.TabIndex = 17;
+            this.btn_Square.Text = "Square";
+            this.btn_Square.UseVisualStyleBackColor = true;
+            this.btn_Square.Click += new System.EventHandler(this.btn_Square_Click);
             // 
             // Klick
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(233, 559);
+            this.ClientSize = new System.Drawing.Size(1178, 552);
+            this.Controls.Add(this.btn_Square);
+            this.Controls.Add(this.dtDataGridView);
+            this.Controls.Add(this.btn_Search);
             this.Controls.Add(this.txt_Y_OLD);
             this.Controls.Add(this.txt_X_OLD);
             this.Controls.Add(this.txt_Ymove);
             this.Controls.Add(this.txt_Xmove);
+            this.Controls.Add(this.txt_SearchLoop);
             this.Controls.Add(this.txt_LOOP);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -169,7 +220,10 @@
             this.MinimizeBox = false;
             this.Name = "Klick";
             this.Text = "Klick";
+            this.Load += new System.EventHandler(this.Klick_Load);
             ((System.ComponentModel.ISupportInitialize)(this.txt_LOOP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_SearchLoop)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,6 +242,10 @@
         private System.Windows.Forms.TextBox txt_X_OLD;
         private System.Windows.Forms.TextBox txt_Ymove;
         private System.Windows.Forms.TextBox txt_Xmove;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.DataGridView dtDataGridView;
+        private System.Windows.Forms.NumericUpDown txt_SearchLoop;
+        private System.Windows.Forms.Button btn_Square;
     }
 }
 
--- Klick/Form1.cs
+++ Klick/Form1.cs
@@ -13,47 +13,70 @@ namespace Klick
 {
     public partial class Klick : Form
     {
-        public const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
-        public const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
+
         [DllImport("user32.dll")]
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInf);
+       
         [DllImport("user32.dll")]
+        
         private static extern bool SetCursorPos(int Posx, int Posy);
+        
+        public const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
+
+        DataTable dt = new DataTable();
 
         int Posx,Posy,Loop,Movex,Movey;
-      
 
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(1000);
 
+            for (int i = 1;  i <= int.Parse(txt_SearchLoop.Text); i++)
+            {
 
+                int PosX = Convert.ToInt32(Cursor.Position.X.ToString());
+                int PosY = Convert.ToInt32(Cursor.Position.Y.ToString());
+
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(PosX + " " + PosY);
+
+                DataRow dr = dt.NewRow();
+                dr[0] = PosX;
+                dr[1] = PosY;
+                dt.Rows.Add(dr);
+
+            }
+        }
+
+        private void Klick_Load(object sender, EventArgs e)
+        {
+
+            dt.Columns.Add("xCord"); 
+            dt.Columns.Add("yCord");
+            dtDataGridView.DataSource = dt;
+        }
+
+        private void btn_Square_Click(object sender, EventArgs e)
+        {
+            GetText();
+
+        }
 
         public Klick()
         {
             InitializeComponent();
-            
-
         }
         
        
         private void btn_Update_Click(object sender, EventArgs e)
         {
-            Posx = int.Parse(txt_X.Text);
-            Posy = int.Parse(txt_Y.Text);
-            Loop = int.Parse(txt_LOOP.Text);
-            Movex = int.Parse(txt_Xmove.Text);
-            Movey = int.Parse(txt_Ymove.Text);
-
+            GetText();
             Loops();
 
         }
-
-
-
-
-
-
         private void Loops ()
         {
-            
             while (true)
             { 
                 SetCur();
@@ -67,7 +90,6 @@ namespace Klick
                 if (Loop == 0)
                     break;
             }
-
         }
         private void SetCur()
         {
@@ -78,11 +100,15 @@ namespace Klick
             System.Threading.Thread.Sleep(20);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
             System.Threading.Thread.Sleep(1100);
+        }   
 
-
+        private void GetText ()
+        {
+            Posx = int.Parse(txt_X.Text);
+            Posy = int.Parse(txt_Y.Text);
+            Loop = int.Parse(txt_LOOP.Text);
+            Movex = int.Parse(txt_Xmove.Text);
+            Movey = int.Parse(txt_Ymove.Text);
         }
-
-        
-    }
-    
+    } 
 }
